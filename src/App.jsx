3b0fd9489118bead77604f2f03510c1c0673ca52
chen <chenczn3528@163.com>
2025-05-13 13:20:41 +0800

    测试旋转

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import Home from './Home';
+// import React from 'react';
+// import Home from './Home';
+//
+// function App() {
+//   return <Home />;
+// }
+//
+// export default App;
+import './App.css';
 
 function App() {
-  return <Home />;
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>这是一段示例文字</h1>
+        <p>竖屏旋转90度</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
 
 
