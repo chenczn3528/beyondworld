70fae4b3a86e9d3929fb925cabd5f138f47b6284
chen <chenczn3528@163.com>
2025-05-14 13:06:01 +0800

    github单页程序(public)1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import App from './App.jsx';
+//
+// // 渲染 App 组件
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 👈 添加这行
 import './index.css';
 import App from './App.jsx';
 
-// 渲染 App 组件
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/beyondworld"> {/* 👈 包裹 App */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
