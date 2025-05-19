497e968dc589ecf53825e55f08a84f0d1339f04f
chen <chenczn3528@163.com>
2025-05-19 15:39:15 +0800

    domain again

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { HashRouter } from 'react-router-dom';
+// import './index.css';
+// import App from './App.jsx';
+//
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//       <App />
+//   </StrictMode>
+// );
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
@@ -6,6 +17,8 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
       <App />
+    </HashRouter>
   </StrictMode>
 );
